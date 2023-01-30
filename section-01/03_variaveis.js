005982ca34a745b5afe430ba91f3d996824564f7
igberZBN <igber_benitez@outlook.com>
2023-01-30 14:46:14 -0300

    Variaveis

---
+++ section-01/03_variaveis.js
@@ -0,0 +1,16 @@
+// existe 3 tipos de variaveis:
+/*
+ *   var
+ *   let
+ *   const
+ */
+
+var test1 = 'hello'; //NÃO USA
+let test2 = 'world'; //usar quando vocẽ quer modificar ou atribuir um valor futuralmente
+const test3 = '!'; //usar quando o valor não vai poder ser alterado
+console.log(test1, test2, test3);
+
+let name;
+console.log(name);
+name = 'gabriel';
+console.log(name);
