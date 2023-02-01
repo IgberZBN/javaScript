b44a79d74c001937236b1ef139d1f0923bbe9cdc
igberZBN <igber_benitez@outlook.com>
2023-02-01 16:29:48 -0300

    Operadores aritmeticos

---
+++ section-01/06_operadoresAritmeticos.js
@@ -0,0 +1,32 @@
+/*  Elementos:
+ *   + adicao ou concatenacao
+ *   - subtracao
+ *   * multiplicacao
+ *   /  divisao
+ *   ** potenciacao
+ *   % resto da divisao
+ */
+
+/* Precendencia:
+ *   ()
+ *   **
+ * * / %
+ *   + -
+ */
+
+// Incrimento
+let contI = 0;
+contI++;
+console.log(contI);
+contI++;
+console.log(contI);
+contI++;
+console.log(contI);
+// Decremento
+let contD = 3;
+contD--;
+console.log(contD);
+contD--;
+console.log(contD);
+contD--;
+console.log(contD);
