443d73db403ff00ae395aca19d53a32959bb3735
igberZBN <igber_benitez@outlook.com>
2023-02-01 16:16:22 -0300

    Dados Primitivos

---
+++ section-01/05_dadosPrimitivos.js
@@ -0,0 +1,11 @@
+// Dados primitivos
+
+const nome = 'Gabrield'; //String
+const num = 10; //Number
+const numFloat = 10.5; //Number
+let nomeFill; //undefine = nao aponta para nenhum local na memoria         /* Nao proposital
+const nulo = null; //null = nao aponta para local nenhum local na memoria     Proposital */
+const booleanoT = true; //boolean
+const booleanoF = false; //boolean
+
+console.log(typeof nome, num, numFloat, nomeFill, nulo, booleanoF, booleanoT);
