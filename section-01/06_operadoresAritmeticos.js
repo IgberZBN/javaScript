0309b398a58493446c067d26e687803963f27d00
igberZBN <igber_benitez@outlook.com>
2023-02-01 16:37:00 -0300

    Converter string para numero

--- section-01/06_operadoresAritmeticos.js
+++ section-01/06_operadoresAritmeticos.js
@@ -30,3 +30,9 @@ contD--;
 console.log(contD);
 contD--;
 console.log(contD);
+
+//Conveter string para number
+const texto = '10';
+console.log(parseFloat(texto)); //converte para um numero com ponto flutuante
+console.log(parseInt(texto)); //converte para um numero inteiro
+console.log(Number(texto)); //conveter automaticamente para inteiro ou flutuante
