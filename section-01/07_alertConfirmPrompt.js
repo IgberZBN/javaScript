9820fd3096366adec4718d8784df61a3fbee1454
igberZBN <igber_benitez@outlook.com>
2023-02-06 13:40:03 -0300

    Exercicio com inputs

--- section-01/07_alertConfirmPrompt.js
+++ section-01/07_alertConfirmPrompt.js
@@ -19,3 +19,8 @@ confirm('Deseja realmente apagar?');
  * * String
  */
 prompt('Digite o seu nome:');
+
+// ? Exercicio, adquirir dois valores do usuario e somar eles
+const num1 = Number(prompt('Digite o primeiro valor'));
+const num2 = Number(prompt('Digite o segundo valor'));
+alert(`O resultado foi ${num1 + num2}`);
