a606d13b2f243e682264f40b02af49105ae1a3d9
igberZBN <igber_benitez@outlook.com>
2023-02-06 13:36:03 -0300

    Alert, confirm e prompt

---
+++ section-01/07_alertConfirmPrompt.js
@@ -0,0 +1,21 @@
+/**
+ * window.alert('Mensagem')
+ * return:
+ * * undefined
+ */
+alert('Mensagem.');
+
+/**
+ * window.confirm('Mensagem')
+ *  return:
+ * * true
+ *  ! false
+ */
+confirm('Deseja realmente apagar?');
+
+/**
+ * window.prompt('Mensagem')
+ * return:
+ * * String
+ */
+prompt('Digite o seu nome:');
