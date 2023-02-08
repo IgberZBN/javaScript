5e9f1a49806fbe514d262a8ee3e9d3184b94e4b0
IgberZBN <igber_benitez@outlook.com>
2023-02-08 16:03:48 -0300

    Problemas com floats

---
+++ section-01/10_maisSobreNumbers.js
@@ -0,0 +1,8 @@
+let num1 = 1;
+let num2 = 2.5;
+let num3 = 5.321321312;
+
+// console.log(num1.toString() + num2);
+// ! console.log(num1.toString(2));  representação não binaria
+// console.log(num3.toFixed(2));
+// console.log(Number.isInteger(num1));
