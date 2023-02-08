43cd32344b4e069e5faabff829e66f18235807c9
IgberZBN <igber_benitez@outlook.com>
2023-02-08 17:13:25 -0300

    Objeto math

---
+++ section-01/11_objetoMath.js
@@ -0,0 +1,13 @@
+let num1 = 9;
+let num2 = 9.5688;
+
+console.log(Math.sqrt(num1)); // Faz a raiz quadradada
+console.log(Math.floor(num2)); // Arredonda para baixo
+console.log(Math.ceil(num2)); // Arredonda para cima
+console.log(Math.round(num2)); // Arredonda para o numero mais proximo
+console.log(Math.max(1, 2, 3, 4, 5, -50, 32, 12, -100, 100.1)); // Mostrar o maior numero
+console.log(Math.min(1, 2, 3, 4, 5, -50, 32, 12, -200, 100.1)); // Mostrar o menor numero
+console.log(Math.random()); // Gera numeros aleatorios entre 0 e 1, mas nunca mostra 1
+console.log(Math.PI); // Valor de pi
+
+console.log(Math.round(Math.random() * (10 - 1) + 1));
