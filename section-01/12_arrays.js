54296692d209b3a5b3b83450e2389edc249808e2
IgberZBN <igber_benitez@outlook.com>
2023-02-10 15:27:19 -0300

    Arrays

---
+++ section-01/12_arrays.js
@@ -0,0 +1,20 @@
+// !               0        2        2
+const alunos = ['Luiza', 'Maria', 'Joao'];
+
+// console.log(typeof alunos);
+// console.log(alunos instanceof Array);
+
+// alunos.push('Gabriel');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, -2));
+// console.log(alunos.slice(0, 2));
+
+// delete alunos[1];
+// alunos.shift;
+// const removido = alunos.pop();
+// alunos.unshift('Luiz');
+// alunos.push('Gabriel');
+// alunos[0] = 'Eduardo';
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
