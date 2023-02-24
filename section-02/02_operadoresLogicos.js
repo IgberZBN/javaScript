23ef6624b4b5a12780ad1e4efe26d19cde9dcfc1
IgberZBN <igber_benitez@outlook.com>
2023-02-24 14:48:09 -0300

    Operadores Logicos

---
+++ section-02/02_operadoresLogicos.js
@@ -0,0 +1,14 @@
+/**
+ * Operadores logicos
+ * && -> AND -> E  - Todas as expressões precisam ser verdadeiras para retornar true
+ * || -> OR -> OU  - Basta uma ser verdadeira para retornar true
+ * ! -> NOT -> Nao - Retorna o valor contrario
+ */
+console.log(true && true); // * true
+console.log(true && false); // ! false
+console.log('');
+console.log(true || false); // * true
+console.log(false || false); // ! false
+console.log('');
+console.log(!false); // * true
+console.log(!true); // ! false
