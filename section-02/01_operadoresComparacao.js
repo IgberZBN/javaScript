faa9fce41a444c047a280a05ef5a9e708823f73c
IgberZBN <igber_benitez@outlook.com>
2023-02-24 14:37:27 -0300

    Operadores de comparação

---
+++ section-02/01_operadoresComparacao.js
@@ -0,0 +1,14 @@
+/**
+ *  Operadores de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= emnor que ou igual a
+ * == igualdade (valor) ****
+ * === igualdade restrita (valor e tipo)
+ * != diferent (valor) ****
+ * !== diferente estrito (valor e tipo)
+ */
+
+const comp = 10 > 5; // vai atribuir o valor na var "comp" com o resultado da comparação com boolean
+console.log(comp);
